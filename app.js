d4946205941a2da1b7e7deea38245e77d53518de
Chad Buie <chadbuie@gmail.com>
2016-09-07 17:24:37 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+
+var express = require('express');
+
+// declare an instance of express
+
+var app = express();
+
+// In production you're going to use PORT 8080, 8888
+
+var port = 5000;
+
+app.get('/', function(req, res){
+	res.render('index', {});
+	res.send('Hello World!');
+});
+
+app.listen(port, function(err){
+	console.log('running server on port ' + port);
+});
+
