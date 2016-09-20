70dc09ed38357e336def52bb1c76646caaee4350
Chad Buie <chadbuie@gmail.com>
2016-09-20 10:53:50 -0400

    Installed and Setup Bower, Bootstrap, Structured front-end dependencies, and set Bower configuration for lib directory.

--- app.js
+++ app.js
@@ -2,11 +2,13 @@
 var express = require('express');
 
 // declare an instance of express
-
 var app = express();
 
-// In production you're going to use PORT 8080, 8888
+// setup middleware, app use gets used by app first and express gets routes second, looks for static file and then set that up
+app.use(express.static('public'));
+app.use(express.static('src/views'));
 
+// In production you're going to use PORT 8080, 8888
 var port = 5000;
 
 app.get('/', function(req, res){
